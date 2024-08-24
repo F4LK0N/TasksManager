4b3eacb57cd6750d4440a807b503b45acef24014
F4LK0N <otaviosoria@gmail.com>
2024-08-24 14:46:25 -0300

    Project structure start

--- api/src/appdjango/app/apps.py
+++ api/src/appdjango/app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+    verbose_name = "Tasks Manager"
+    
+    def ready(self):
+        pass
