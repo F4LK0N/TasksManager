bbc944407bd4dc1184496fd5162ef4136b53da21
F4LK0N <otaviosoria@gmail.com>
2024-08-24 09:30:56 -0300

    API - DJango Start

--- api/src/config/urls.py
+++ api/src/config/urls.py
@@ -8,6 +8,7 @@ router.register(r'tasks', TaskViewSet)
 router.register(r'works', WorkViewSet)
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
